8ada5a7677d86c4d26b9ebafec3d889380dd781e
eozuna3 <eo30239@gmail.com>
2020-06-07 16:46:55 -0500

    Updates

---
+++ precache-manifest.6a1b4e6bb5a3e81e4162924dc033b038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cf4cfc2f1172dd2d8cd54615e2151d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f306ab5c0b16597f96b3",
+    "url": "/clicky-game/static/css/main.ca7b99c9.chunk.css"
+  },
+  {
+    "revision": "37151455a66730524ab4",
+    "url": "/clicky-game/static/js/2.8072629e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.8072629e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f306ab5c0b16597f96b3",
+    "url": "/clicky-game/static/js/main.eabab5b5.chunk.js"
+  },
+  {
+    "revision": "ddea091f236f02df6e9c",
+    "url": "/clicky-game/static/js/runtime-main.de0ba425.js"
+  }
+]);
